ab70ae494f53f94202575ff947051e6c9f8934b4
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-10-14 10:39:57 +0000

    コントローラー、ルーティングの作成

---
+++ app/controllers/public/threads_controller.rb
@@ -0,0 +1,2 @@
+class Public::ThreadsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,31 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   
+  namespace :user do
+      get '/users/unsubscribe' => 'users#unsubscribe'
+      get 'users/mypage' => 'users#show'
+      get '/users/information/edit' => 'users#edit'
+      patch '/users' => 'users#update'
+      patch '/users/withdraw' => 'users#withdraw'
+      resources :reviews
+      resources :boards, only: [:index, :show, :create]
+      resources :threads
+      resources :comments, only: [:create, :edit, :update]
+      
+  
   #管理者用
   #URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
+  
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :reviews
+    resources :boards, only: [:index]
+    resources :comments
+    resources :threads
+  end
   
   root to: 'homes#top'
   
