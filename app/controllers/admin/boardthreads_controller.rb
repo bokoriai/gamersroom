09ad00e4355b0b378bea9fe7c9ef749a685aebd4
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-11-05 18:43:45 +0000

    adminのTOPに新着スレッド一覧遷移先追加、新着スレッド一覧、詳細画面作成、感情分析が0.0未満の場合スレッド削除ボタンを表示

---
+++ app/controllers/admin/boardthreads_controller.rb
@@ -0,0 +1,22 @@
+class Admin::BoardthreadsController < ApplicationController
+    def index
+        now = Time.current
+        @boardthreads = Boardthread.where(created_at: now.prev_day...now).order(created_at: :desc)
+    end
+    
+    def show
+        @boardthread = Boardthread.find(params[:id])
+    end
+    
+    def destroy
+        @boardthread = Boardthread.find(params[:id])
+        @boardthread.destroy
+        redirect_to admin_boardthreads_path
+    end
+    
+    private
+    def boardthread_params
+        params.require(:boardthread).permit(:title, :body, :category, :score)
+    end
+        
+end
---
+++ test/controllers/admin/boardthreads_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Admin::BoardthreadsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
