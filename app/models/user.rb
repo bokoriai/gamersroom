9a64d5e8a4ccd4f8fc0d73bd612687eacbe50563
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-12-05 19:14:03 +0000

    プロフィール画像が表示されないエラーの対応２（エラー対応完了）

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
          
   validates :email, presence: true
-  validates :password, presence: true
+  
          
   has_one_attached :profile_image
   
@@ -21,7 +21,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [x, y]).processed
+    profile_image.variant(resize_to_limit: [x, y])
   end
   
   def active_for_authentication?
